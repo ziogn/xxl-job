4bb55fbbfce91375854673e3be48852b38a6b012
ziogn <xiong04@foxmail.com>
2024-02-13 22:49:39 +0800

    feat: xxlJob二开

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/model/XxlJobGroup.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/model/XxlJobGroup.java
@@ -1,9 +1,14 @@
 package com.xxl.job.admin.core.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import com.xxl.job.admin.core.util.SpringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by xuxueli on 16/9/30.
@@ -20,10 +25,31 @@ public class XxlJobGroup {
     // registry list
     private List<String> registryList;  // 执行器地址列表(系统注册)
     public List<String> getRegistryList() {
-        if (addressList!=null && addressList.trim().length()>0) {
-            registryList = new ArrayList<String>(Arrays.asList(addressList.split(",")));
+        if (addressList!=null && !addressList.trim().isEmpty()) {
+            //address执行器管理填入的机器地址 http://ip:port这里需要兼容 lb://serviceName地址
+            if (addressList.contains("lb://")) {
+                addressList = Arrays.stream(addressList.split(","))
+                        .filter(Objects::nonNull)
+                        .filter(s -> !s.trim().isEmpty())
+                        .map(this::obtainUrl)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.joining(","));
+            }
+            registryList = new ArrayList<>(Arrays.asList(addressList.split(",")));
         }
         return registryList;
+    }
+
+    public List<String> obtainUrl(String address){
+        if (address.startsWith("lb://")) {
+            String serviceName = address.replace("lb://", "");
+            DiscoveryClient discoveryClient = SpringUtils.getBean(DiscoveryClient.class);
+            List<ServiceInstance> instances = discoveryClient.getInstances(serviceName);
+            return instances.stream()
+                    .map(i -> i.getUri().toString())
+                    .collect(Collectors.toList());
+        }
+        return List.of(address);
     }
 
     public int getId() {
